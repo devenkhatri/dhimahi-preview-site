197076576dbee60be3e291f6985ba4c33b8dfcf8
Harshna Goratela <harshna.goratela@gmail.com>
2025-08-25 13:24:17 +0530

    fixed build issue

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   experimental: {
     optimizePackageImports: ["lucide-react"]
+  },
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
   }
 };
 export default nextConfig;
